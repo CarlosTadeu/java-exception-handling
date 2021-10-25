17627e7af7f9dbd251b89e1d75953a2fbe6fe626
Carlos Tadeu <carlos.santos@opus-software.com.br>
2021-10-25 15:51:50 -0300

    Initial commit

---
+++ src/main/java/com/example/javaexceptionhandling/JavaExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExceptionHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExceptionHandlingApplication.class, args);
+    }
+
+}
